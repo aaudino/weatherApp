6a868ed69244eee0592ad151d703ff9407e09c6a
aaudino <a.audino95@gmail.com>
2022-09-04 12:26:37 +0200

    improved error handling during API calls

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-import {keys} from "./confidentials.js";
+import { keys } from "./confidentials.js";
 
 const search = document.querySelector(".search-icon");
 const close = document.querySelector(".close-icon");
 const inputField = document.querySelector(".inputBox__inputfield");
 const citiesContainer = document.querySelector(".searchbar__cities");
-let cityBtns ;
+let cityBtns;
 let cityWrappers;
-const weatherContainer = document.querySelector(".weatherContainer__grid")
-const startBtn = document.querySelector(".startBtn")
-
+const weatherContainer = document.querySelector(".weatherContainer__grid");
+const startBtn = document.querySelector(".startBtn");
 
 search.addEventListener("click", () => {
   search.classList.add("search-icon--openSB");
@@ -31,8 +30,6 @@ close.addEventListener("click", () => {
   }, 200);
 });
 
-
-
 //Adding new Cities
 inputField.addEventListener("keypress", function (e) {
   if (e.key === "Enter") {
@@ -43,106 +40,111 @@ inputField.addEventListener("keypress", function (e) {
   </div>`;
     citiesContainer.insertAdjacentHTML("beforeend", newCityContainer);
     inputField.value = null;
- }
+  }
 });
 
-
-
-citiesContainer.addEventListener("click", (cc) =>{
+citiesContainer.addEventListener("click", (cc) => {
   console.log(cc.target);
-  const clicked = cc.target
-  if (clicked.classList.contains("city") ) {
-   console.log("city button clicked");
-   console.log("here is the Sibling");
-   console.log(clicked.nextElementSibling);
-   const trashIcon = clicked.nextElementSibling
-   trashIcon.classList.toggle("trash-icon--closed")
+  const clicked = cc.target;
+  if (clicked.classList.contains("city")) {
+    console.log("city button clicked");
+    console.log("here is the Sibling");
+    console.log(clicked.nextElementSibling);
+    const trashIcon = clicked.nextElementSibling;
+    trashIcon.classList.toggle("trash-icon--closed");
   }
   if (clicked.classList.contains("trash-icon")) {
     console.log("icon Clicked");
-    const parentElement = clicked.parentElement
+    const parentElement = clicked.parentElement;
     console.log(parentElement);
-    citiesContainer.removeChild(parentElement)
+    citiesContainer.removeChild(parentElement);
   }
-})
-
-
+});
 
 // API Picture call
 async function getCityImage(userCity) {
   console.log("Image function started");
-  let auth_key = btoa(
-    `${keys.roadgoat.key}:${keys.roadgoat.secretKey}`
-  );
+  let auth_key = btoa(`${keys.roadgoat.key}:${keys.roadgoat.secretKey}`);
   const endpoint = new URL(
     `https://api.roadgoat.com/api/v2/destinations/auto_complete?q=${userCity}`
   );
-  //console.log(endpoint.toString());
   const response = await fetch(endpoint, {
     headers: {
-      'Authorization': `Basic ${auth_key}`,
+      Authorization: `Basic ${auth_key}`,
     },
-
   });
-  //console.log(response);
-  const data =  await response.json()
+  const data = await response.json();
   console.log(data);
-  let picture = data.included[0].attributes.image.full
-  let long = data.data[0].attributes.longitude
-  let lat = data.data[0].attributes.latitude
-  const dataArray = [picture,long,lat]
-  return dataArray
+  if (!data.data[0]) {
+    throw new Error("Can not find the desired city");
+  }
+  const {
+    data: [
+      {
+        attributes: { longitude, latitude },
+      },
+    ],
+    included: [
+      {
+        attributes: {
+          image: { full: picture },
+        },
+      },
+    ],
+  } = data;
+  console.log("check");
+  console.log(longitude, latitude, picture);
+
+  console.log("checkover");
+  const dataArray = [picture, longitude, latitude];
+  console.log(dataArray);
+  return dataArray;
 }
 
-
-async function getWeatherData(city) {
-  console.log("start function");
-  let [img,long,lat] = await getCityImage(city)
-  console.log("function complete");
-  console.log("here is the data");
-  console.log(img,long,lat);
-  console.log("WeatherData function started");
-  const endpoint = new URL(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${keys.openweatherMap.key}`)
-  const response = await fetch(endpoint)
+async function getWeatherData(latitude, longitude) {
+  const endpoint = new URL(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${keys.openweatherMap.key}`
+  );
+  const response = await fetch(endpoint);
   //console.log(response);
-  const data = await response.json()
+  const data = await response.json();
   console.log(data);
-  const temp = data.main.temp
-  const tempMin = data.main.temp_min
-  const tempMax = data.main.temp_max
-  console.log(temp,tempMin,tempMax);
-  const finalData = [img,temp,tempMin,tempMax]
-  return finalData
-
+  const temp = data.main.temp;
+  const tempMin = data.main.temp_min;
+  const tempMax = data.main.temp_max;
+  const finalData = [temp, tempMin, tempMax];
+  return finalData;
 }
 
-async function createHtml(city) {
-  let [img,temp,tempMin,tempMax]= await getWeatherData(city)
+async function createHtml(picture, temperature, maximumTemp, minimumTemp) {
   const html = `<div class="grid__item">
-  <img class = "grid__item--img" src="${img}" alt="">
+  <img class = "grid__item--img" src="${picture}" alt="">
   <img  class ="grid__item--emoji grid__item--sun" src= "img/sun.png" alt="">
   <img  class ="grid__item--emoji grid__item--up"src="img/upTriangle.png" alt="">
   <img  class ="grid__item--emoji grid__item--down"src="img/downTriangle.png" alt="">
-  <p class="grid__item--info grid__item--temp"> ${temp}° </p>
-  <p class="grid__item--info grid__item--maxTemp">${tempMax}°</p>
-  <p class="grid__item--info grid__item--minTemp">${tempMin}°</p>
-</div>`
+  <p class="grid__item--info grid__item--temp"> ${temperature}° </p>
+  <p class="grid__item--info grid__item--maxTemp">${maximumTemp}°</p>
+  <p class="grid__item--info grid__item--minTemp">${minimumTemp}°</p>
+</div>`;
 
-weatherContainer.insertAdjacentHTML('beforeend',html)
+  weatherContainer.insertAdjacentHTML("beforeend", html);
 }
 
-
-
-
-startBtn.addEventListener("click",function () {
-cityBtns = document.querySelectorAll(".city");
-cityWrappers = document.querySelectorAll(".cityWrapper")
-while(weatherContainer.childElementCount > 0){
- weatherContainer.removeChild(weatherContainer.firstChild)}
-cityBtns.forEach((cb) =>{
-  let search = cb.innerHTML.toLowerCase()
-  console.log(search);
-  //getWeatherData(search)
-  createHtml(search)
-})
-})
+startBtn.addEventListener("click", function () {
+  cityBtns = document.querySelectorAll(".city");
+  cityWrappers = document.querySelectorAll(".cityWrapper");
+  while (weatherContainer.childElementCount > 0) {
+    weatherContainer.removeChild(weatherContainer.firstChild);
+  }
+  cityBtns.forEach(async (cb) => {
+    let search = cb.innerHTML.toLowerCase();
+    try {
+      const [img, long, lat] = await getCityImage(search);
+      const [temp, tempMin, tempMax] = await getWeatherData(lat, long);
+      createHtml(img, temp, tempMax, tempMin);
+    } catch (err) {
+      console.log("an error has occured");
+      console.log(err);
+    }
+  });
+});
